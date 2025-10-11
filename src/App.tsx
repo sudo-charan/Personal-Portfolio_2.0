304ec39162f084efc65dbc15b517107fe2c1e190
Charan Naik <charanaikofficial@gmail.com>
2025-10-11 22:21:08 +0530

    vercel web analytics package added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import LoadingScreen from "./components/LoadingScreen";
 import Index from "./pages/Index";
 import Profile from "./pages/Profile";
@@ -36,6 +37,7 @@ const App = () => {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <Analytics />
       </TooltipProvider>
     </QueryClientProvider>
   );
